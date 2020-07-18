01d8e277732a9657539830fcb70f197de7d408a0
Tails Era <TailsEraYT@hostingwebsites.tk>
2020-07-17 22:23:19 -0400

    Texts Channels Are now avalible to create, Working on Voice channels Now.

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const discord = require("discord.js");
 const client = new discord.Client();
 require('dotenv').config();
+const guild = new discord.Guild(client);
+
 const embed = {
     info: require("./Messages/info"),
 }
@@ -9,46 +11,68 @@ client.on("ready", () => {
     console.log("Ready to start servers.");
 });
 
-client.on("message", async message=>0);{
-    if (!message.content.startsWith(prefix)) return;
+function catchErr (err, message) {
+  message.channel.send ("there was an error @" + message.channel, "in guild" + message.guild);
+  message.channel.send ("Here is the Error. ```" + err + "```");
+  console.log("an error occured while running." + err + "try to fix this ASAP." )
+}
+
+function createChannel (guild, message, args) {
+  message.guild.channels.create(args[1], {
+    type: 'text',
+    permissionOverwrites: [
+        {
+            id: message.guild.id,
+            allow: ['VIEW_CHANNEL'],
+        }]
+    })
+          return message.channel.send("Creating Channel:" + args[1]);
+}
+
+client.on('shardError', error => {
+  console.error('A websocket connection encountered an error:', error);
+});
+
+client.on("message", (message) => {
+try {
+  if ( message.author.bot || !message.content.startsWith(prefix)) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const cmd = args.shift().toLowerCase();
 
-    if (cmd === info) {
-        message.channel.send({embed: embed.info.embed });
+    if (cmd === "info") {
+        return message.channel.send({embed: embed.info.embed });
     }
 
-    if (cmd === channel) {
+    if (cmd === "restart") {
+      message.channel.send("Restarting...");
+      return bot.destroy();
+    }
+
+    if (cmd === "channel") {
         if (args.length < 1) return message.channel.send("> S-channel (Create / Delete) (Channel Name)");
         
         switch (args[0]) {
                 
-            case "list":
-            if (args.length > 1) {
-              switch (args[1]) {
-                case "1":
-                    var server = message.guild;
-                    var ChannelName = args[3];
-                    if (client.textChannel === args[3]) {
-                        return message.channel.send("> ERROR: Unable to Create, Channel Already Exists.");
-                    }
-                    else {
-                    server.createChannel(args[3], "text");
-                  return message.channel.send("Creating Channel:", ChannelName);
-                    };
-                default:
-                  //This Deals a case where no Channel Name Was Given.
-                  break;
-              }
-            } else {
-              return message.channel.send("> No Name Selected, Unable to continue.");
-            }`  `
-            break;
-          default:
-            return message.channel.send("> Unknown Argument. Are you sure you did this correctly?");
+          case "create":
+            if (client.textChannel === args[1]) {
+                return message.channel.send("> ERROR: Unable to Create, Channel Already Exists.");
+            }
+            else {
+              createChannel(guild, message, args);
+            };
+        default:
+          //This Deals a case where no Channel Name Was Given.
+          break;
         }
     }
 
-};
+    
+  }
+  catch (err) {
+    catchErr(err, message);
+  }
+  });
+
+  
 
 client.login(process.env.token);
